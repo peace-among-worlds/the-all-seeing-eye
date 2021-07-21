6545ab4e4022fd711724ef0bc308b909874c8a9f
peace-among-worlds <34693070+peace-among-worlds@users.noreply.github.com>
2021-07-22 11:00:23 +1200

    update app.js security

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
@@ -15,10 +14,6 @@ import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
-
-const app = new Clarifai.App({
- apiKey: '8ae71bf70d07478390bdc2c21aff0350'
-});
 
 const particlesOptions = {
   particles: {
@@ -87,11 +82,14 @@ loadUser = (data) => {
 
  onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL,
-        this.state.input)
-      .then(response => {
+        fetch('http://localhost:3000/imageurl', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        input: this.state.input 
+      }) 
+    })
+      .then(response => response.json(){
         if (response) {
           fetch('http://localhost:3000/image', {
             method: 'put',
